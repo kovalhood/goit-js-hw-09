c47c08424ce286457542ce21f02fe41acea2ae87
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-01-31 15:07:55 +0200

    Task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector('body');
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+let colorChangeInterval = null;
+
+startButton.addEventListener('click', startColorChange);
+stopButton.addEventListener('click', stopColorChange);
+
+function startColorChange() {
+    colorChangeInterval = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+        startButton.disabled = true;
+        stopButton.disabled = false;
+    }, 1000);
+};
+
+function stopColorChange() {
+    clearInterval(colorChangeInterval);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+};
